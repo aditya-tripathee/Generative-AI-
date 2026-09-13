9ba4a92b8ab333e2d908c10580ae65e8f5bd4bb5
aditya-tripathee <adityatripatheee@gmail.com>
2026-09-13 11:44:26 +0530

    add parallel LangChain chain with Mistral AI and environment configuration

--- RAG_sequence/parallel.py
+++ RAG_sequence/parallel.py
@@ -1,10 +1,14 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
+
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableParallel
+from langchain_core.runnables import RunnableParallel, RunnableLambda
 
 # 2. Model
 model = ChatMistralAI(model="open-mistral-7b")
@@ -18,7 +22,7 @@ short_prompt = ChatPromptTemplate.from_messages(
         ("user", "{question}"),
     ]
 )
-detailed_prompt = ChatPromptTemplate.from_messages(
+detailed_prompt = ChatPromptTemplate.from_messages( 
     [
         ("system", "You are a helpful assistant. Give detailed answers."),
         ("user", "{question}"),
@@ -28,11 +32,16 @@ detailed_prompt = ChatPromptTemplate.from_messages(
 # formatted_detailed = detailed_prompt.format(question="What is the capital of France?")
 
 chain = RunnableParallel(
-    short=short_prompt | model | output_parser,
-    detailed=detailed_prompt | model | output_parser,
+    short=RunnableLambda(lambda x: x["short"]) | short_prompt | model | output_parser,
+    detailed=RunnableLambda(lambda x: x["detailed"]) | detailed_prompt | model | output_parser,
 )
 
 # Run the chain
-response = chain.invoke({"question": "What is the capital of France?"})
+response = chain.invoke(
+    {
+        "short": "What is the capital of France?",
+        "detailed": "What is the capital of Kathmandu Nepal?"
+    }
+    )
 print("Response:", response)
-print("Type of response:", type(response))  
+print("Type of response:", type(response))
